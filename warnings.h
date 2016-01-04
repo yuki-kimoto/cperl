d00193de8ae696015f51717accfa96b57c4eb15e
Reini Urban <rurban@cpanel.net>
2016-01-04 14:34:46 +0100

    signatures: extend typing on args, warn, castable
    use warnings "types" warns when an explicit typecheck needs
    to insert a cast. This is usually caused by a wrong method argument.
    
    add OP* op_typechecked to avoid run-time typechecks.
    
    type casts can be inserted only for Scalar => Bool/Numeric.
    castable = (dtyp > type_Str && dtyp < type_Object)
    
    add subtyping restrictions to pod/perltypes for proper inheritance rules
    satifying the Liskov substitution principles.

--- warnings.h
+++ warnings.h
@@ -113,9 +113,13 @@
 #define WARN_MISSING			 66
 #define WARN_REDUNDANT			 67
 
-#define WARNsize			 17
-#define WARN_ALLstring			 "\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125"
-#define WARN_NONEstring			 "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+/* Warnings Categories added in Perl 5.022 */
+
+#define WARN_TYPES			 68
+
+#define WARNsize			 18
+#define WARN_ALLstring			 "\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125"
+#define WARN_NONEstring			 "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
 
 #define isLEXWARN_on 	(PL_curcop->cop_warnings != pWARN_STD)
 #define isLEXWARN_off	(PL_curcop->cop_warnings == pWARN_STD)
